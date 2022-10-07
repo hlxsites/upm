30aaf29b984be2c62098c93d0657086b92ac51ba
Hannes Hertach <hanneshertach@gmail.com>
2022-10-07 11:30:28 +0200

    remove redundant test

--- test/blocks/header/header.test.js
+++ test/blocks/header/header.test.js
@@ -1,8 +1,6 @@
 /* eslint-disable no-unused-expressions */
-/* global describe it */
 
 import { readFile } from '@web/test-runner-commands';
-import { expect } from '@esm-bundle/chai';
 
 document.body.innerHTML = await readFile({ path: '../../scripts/dummy.html' });
 
@@ -20,26 +18,4 @@ const headerBlock = buildBlock('header', [['Nav', '/test/blocks/header/nav']]);
 document.querySelector('header').append(headerBlock);
 decorateBlock(headerBlock);
 await loadBlock(headerBlock);
-await sleep();
-
-describe('Header block', () => {
-  it('Hamburger shows and hides nav', async () => {
-    const hamburger = document.querySelector('.header .nav-hamburger');
-    const nav = document.querySelector('.header nav');
-    expect(hamburger).to.exist;
-    expect(nav).to.exist;
-    hamburger.click();
-    expect(nav.getAttribute('aria-expanded')).to.equal('true');
-    hamburger.click();
-    expect(nav.getAttribute('aria-expanded')).to.equal('false');
-  });
-
-  it('Section title shows and hides section', async () => {
-    const sections = document.querySelector('.header .nav-sections');
-    const title = sections.querySelector(':scope li');
-    title.click();
-    expect(title.getAttribute('aria-expanded')).to.equal('true');
-    title.click();
-    expect(title.getAttribute('aria-expanded')).to.equal('false');
-  });
-});
+await sleep(1000);
