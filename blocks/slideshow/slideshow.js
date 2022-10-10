b91fb9ca41fdf19324daca23ef830b26a8aa63b5
Hannes Hertach <hanneshertach@gmail.com>
2022-10-10 10:56:57 +0200

    wip: slideshow

--- blocks/slideshow/slideshow.js
+++ blocks/slideshow/slideshow.js
@@ -12,23 +12,39 @@ export default async function decorate($block) {
   const $controlsContainer = document.createElement('div');
   $controlsContainer.classList.add('controls-container');
   $controlsContainer.innerHTML = `
-    <button name="next">Next</button>
     <button name="prev">Prev</button>
+    <button name="next">Next</button>
   `;
-  $block.append($controlsContainer);
+  $block.prepend($controlsContainer);
 
   const nextButton = $controlsContainer.querySelector('button[name="next"]');
   const prevButton = $controlsContainer.querySelector('button[name="prev"]');
 
   nextButton.addEventListener('click', () => {
     const currentIndex = getCurrentSlideIndex($block);
+    const neg1 = (((currentIndex - 1) % numChildren) + numChildren) % numChildren;
+    const add1 = (currentIndex + 1) % numChildren;
+    const add2 = (currentIndex + 2) % numChildren;
+
+    $block.children[neg1].removeAttribute('prev');
     $block.children[currentIndex].removeAttribute('active');
-    $block.children[(currentIndex + 1) % numChildren].setAttribute('active', true);
+    $block.children[currentIndex].setAttribute('prev', true);
+    $block.children[add1].setAttribute('active', true);
+    $block.children[add1].removeAttribute('next');
+    $block.children[add2].setAttribute('next', true);
   });
 
   prevButton.addEventListener('click', () => {
     const currentIndex = getCurrentSlideIndex($block);
+    const neg2 = (((currentIndex - 2) % numChildren) + numChildren) % numChildren;
+    const neg1 = (((currentIndex - 1) % numChildren) + numChildren) % numChildren;
+    const add1 = (currentIndex + 1) % numChildren;
+
+    $block.children[add1].removeAttribute('next');
     $block.children[currentIndex].removeAttribute('active');
-    $block.children[(((currentIndex - 1) % numChildren) + numChildren) % numChildren].setAttribute('active', true);
+    $block.children[currentIndex].setAttribute('next', true);
+    $block.children[neg1].setAttribute('active', true);
+    $block.children[neg1].removeAttribute('prev');
+    $block.children[neg2].setAttribute('prev', true);
   });
 }
