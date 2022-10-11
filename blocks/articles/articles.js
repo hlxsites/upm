c9deea3c89885852dcd3f92c5f5d7267946462fb
Duy Nguyen <dunguyen@adobe.com>
2022-10-11 10:45:58 +0700

    categorize articles

--- blocks/articles/articles.js
+++ blocks/articles/articles.js
@@ -5,20 +5,25 @@ import {
 
 export default async function decorate(block) {
   const config = readBlockConfig(block);
+  const layout = config?.layout || 'news';
   block.innerHTML = '';
 
   const newsIndex = await fetchIndex('query-index');
 
   let rawNews = newsIndex.data;
-  rawNews = rawNews.filter((news) => news.tags.includes('article'));
+  rawNews = rawNews.filter((news) => news.tags.includes(layout));
 
   rawNews.forEach((newsItem) => {
     const eachNews = document.createElement('div');
     eachNews.classList.add('news');
-    eachNews.innerHTML = `
+    if(newsItem.image) {
+      eachNews.innerHTML += `
       <a href="${newsItem.path}" target="_blank">
         <img src="${newsItem.image}" />
       </a>
+      `;
+    }
+    eachNews.innerHTML += `
       <h2>${newsItem.title}</h2>
       <a href="${newsItem.path}" target="_blank">Read More</a>
     `;
